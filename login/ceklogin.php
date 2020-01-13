a808f1bf29ddf7364185229f20eb6cb3b9e1fd7f
nksari01 <58420977+nksari01@users.noreply.github.com>
2020-01-13 17:42:53 +0700

    up login

---
+++ login/ceklogin.php
@@ -0,0 +1,45 @@
+<?php
+session_start(); //mengawali session
+include "Koneksi.php"; //menghubungkan dg koneksi
+$error = '';
+    if (isset($_POST['submit'])) { 
+        $username = $_POST["username"];
+    $password= $_POST["password"];
+    $ambil = $koneksi->query("SELECT * FROM user WHERE username='$username' AND password='$password'");
+
+    $akuncocok = $ambil->num_rows;
+
+       if ($akuncocok==1)
+     {
+        //mendapat akun dalam array
+        $akun= $ambil->fetch_assoc();
+            // simpan di session customer/user
+        $_SESSION["user"] = $akun;
+        echo "<script>alert('sukses login'); </script>";
+                $_SESSION['akses'] = $akun['id_akses']; //check kesekuaian akses dg session
+
+                if($akun['id_akses']=="1"){
+                    header("location:/bismillahok/baradmin/admin/index.php"); //lokasi target jika berhasil login
+                }
+
+                elseif($akun['id_akses']=="2"){
+        if (isset($_SESSION["cart"]) OR !empty($_SESSION["cart"])) 
+    {
+    echo "<script>location='/bismillahok/checkout.php';</script>";
+    }
+    else
+    {
+        echo "<script>location='/bismillahok/index.php';</script>";
+    }
+
+                // header("location:/bismillahok/checkout.php"); //lokasi target jika berhasil login
+                }
+               else
+    {
+
+        echo "<script>alert('gagal login silahkan masuk lagi'); </script>";
+        echo "<script>location='login.php';</script>";
+    }
+}
+}
+?>
