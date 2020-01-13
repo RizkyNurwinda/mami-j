94f6ab65737987bf43e6401b0eb62e6563cc8b5d
RizkyNurwinda <Rizkynurwinda@gmail.com>
2020-01-13 21:48:35 +0700

    edit logput admin

--- baradmin/admin/logout.php
+++ baradmin/admin/logout.php
@@ -4,7 +4,7 @@ session_start();
 if(session_destroy()) // Menghapus Sessions
 {
 echo "<script>alert('anda telah logout');</script>";
-echo "<script>location='/bismillahok/index.php';</script>";
+echo "<script>location='/mami-j/index.php';</script>";
 
 }
 ?>
