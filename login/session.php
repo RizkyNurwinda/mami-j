8f46818c9d4022029185e60f1e457b0cc826b3ca
anisa231299 <anisasaf5@gmail.com>
2020-01-13 18:58:34 +0700

    up login anisa

---
+++ login/session.php
@@ -0,0 +1,14 @@
+<?php
+error_reporting(0);
+include "Koneksi.php"; //mengarah pada koneksi.php
+session_start(); //mengawali session
+
+$user_check = $_SESSION['USERNAME'];  //mengecheck kesesuaian session
+
+$sql = "select * from user where username='$user_check'"; //menghubungkan session dengan db
+$ses_sql = mysqli_query($koneksi, $sql); 
+$row = mysqli_fetch_assoc($ses_sql);
+$login_session = $row['id']; //mengakses session login dg id
+$akses = $row['akses']; //koreksi akses
+$pass = $row['PASSWORD']; //koreksi password
+ ?>
