fa09b6e3f58cc1048f014b545a211fc73953f087
RizkyNurwinda <Rizkynurwinda@gmail.com>
2020-01-14 10:57:52 +0700

    update

--- baradmin/admin/rekap_data.php
+++ baradmin/admin/rekap_data.php
@@ -54,111 +54,131 @@ include 'koneksi.php';
             <!-- HEADER DESKTOP-->
 
             <!-- MAIN CONTENT-->
-            <div class="main-content">
-                <div class="section__content section__content--p30">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-md-12">
-                                  <div class="overview-wrap">
-                                     <div id="page-wrapper" >
-            <div id="page-inner">
-                                      
+             <div id="wrapper">
+           
+         <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
                                   
 <br>
-  <h2 class="title-1">LAPORAN PEMBELIAN</h2>
-<form method="get" action="">
-       <!--  <label>Filter Berdasarkan</label><br>
-        <select name="filter" id="filter">
-            <option value="">Pilih</option>
-            <option value="1">Per Tanggal</option>
-            <option value="2">Per Bulan</option>
-            <option value="3">Per Tahun</option>
-        </select>
-        <br /><br />
+   <div class="container">
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">LAPORAN PEMBELIAN</h1>
+          </div>
 
-        <div id="form-tanggal">
-            <label>Tanggal</label><br>
-            <input type="text" name="tanggal" class="input-tanggal" />
-            <br /><br />
-        </div>
 
-        <div id="form-bulan">
-            <label>Bulan</label><br>
-            <select name="bulan">
-                <option value="">Pilih</option>
-                <option value="1">Januari</option>
-                <option value="2">Februari</option>
-                <option value="3">Maret</option>
-                <option value="4">April</option>
-                <option value="5">Mei</option>
-                <option value="6">Juni</option>
-                <option value="7">Juli</option>
-                <option value="8">Agustus</option>
-                <option value="9">September</option>
-                <option value="10">Oktober</option>
-                <option value="11">November</option>
-                <option value="12">Desember</option>
-            </select>
-            <br /><br />
-        </div>
+                    <form method="get" action="">
+                      <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">Filter Berdasarkan</label>
+                        <div class="col-sm-5">
+                          <select class="form-control" name="filter" id="filter">
+                              <option value="" selected="">Pilih</option>
+                              <option value="1">Per Tanggal</option>
+                              <option value="2">Per Bulan</option>
+                              <option value="3">Per Tahun</option>
+                          </select>
+                        </div>
+                      </div>
 
-        <div id="form-tahun">
-            <label>Tahun</label><br>
-            <select name="tahun">
-                <option value="">Pilih</option>
-                <?php
-                $query = "SELECT YEAR(tanggal_kirim) AS tahun FROM pembelian GROUP BY YEAR(tanggal_kirim)"; // Tampilkan tahun sesuai di tabel transaksi
-                $sql = mysqli_query($koneksi, $query); // Eksekusi/Jalankan query dari variabel $query
+                      <div class="form-group row" id="form-tanggal">
+                        <label class="col-sm-2 col-form-label">Tanggal</label>
+                        <div class="col-sm-5">
+                            <input type="date" name="tanggal" class="form-control" size="4"/><!-- class="input-tanggal"  -->
+                        </div>
+                      </div>
 
-                while($data = mysqli_fetch_array($sql)){ // Ambil semua data dari hasil eksekusi $sql
-                    echo '<option value="'.$data['tahun'].'">'.$data['tahun'].'</option>';
-                }
-                ?>
-            </select>
-            <br /><br />
-        </div>
+                        <div class="form-group row" id="form-bulan">
+                          <label class="col-sm-2 col-form-label">Bulan</label>
+                          <div class="col-sm-5">
+                            <select class="form-control" name="bulan">
+                                <option value="">Pilih</option>
+                                <option value="1">Januari</option>
+                                <option value="2">Februari</option>
+                                <option value="3">Maret</option>
+                                <option value="4">April</option>
+                                <option value="5">Mei</option>
+                                <option value="6">Juni</option>
+                                <option value="7">Juli</option>
+                                <option value="8">Agustus</option>
+                                <option value="9">September</option>
+                                <option value="10">Oktober</option>
+                                <option value="11">November</option>
+                                <option value="12">Desember</option>
+                            </select>
+                          </div>
+                        </div>
 
-           <button class="btn btn-primary" type="submit"><i class="glyphicon glyphicon-floppy-disk"></i> Tampilkan</button>
-        <a href="rekap_data.php">Reset Filter</a>
-    </form>
-     <hr /> -->
+                        <div class="form-group row" id="form-tahun">
+                          <label class="col-sm-2 col-form-label">Tahun</label>
+                          <div class="col-sm-5">
+                            <select class="form-control" name="tahun">
+                                <option value="">Pilih</option>
+                                    <?php
+                                    $query = "SELECT YEAR(tanggal_beli) AS tahun FROM pembelian GROUP BY YEAR(tanggal_beli)"; // Tampilkan tahun sesuai di tabel transaksi
+                                    $sql = mysqli_query($koneksi, $query); // Eksekusi/Jalankan query dari variabel $query
+
+                                    while($data = mysqli_fetch_array($sql)){ // Ambil semua data dari hasil eksekusi $sql
+                                        echo '<option value="'.$data['tahun'].'">'.$data['tahun'].'</option>';
+                                    }
+                                    ?>
+                            </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                          <label class="col-sm-2 col-form-label">&nbsp;</label>
+                          <div class="col-sm-10">
+                            <button type="submit" class="btn btn-primary" value="">Tampilkan</button>
+                            <a href="rekap_data.php" class="btn btn-danger">Reset Filter</a>
+                          </div>
+                        </div>
+
+                        <!-- <button type="submit">Tampilkan</button>
+                        <a href="rekap_data.php">Reset Filter</a> -->
+                    </form>
+     <hr />
 
     <?php
     if(isset($_GET['filter']) && ! empty($_GET['filter'])){ // Cek apakah user telah memilih filter dan klik tombol tampilkan
         $filter = $_GET['filter']; // Ambil data filder yang dipilih user
-
+        
         if($filter == '1'){ // Jika filter nya 1 (per tanggal)
+            
             $tgl = date('d-m-y', strtotime($_GET['tanggal']));
 
-            echo '<b>Data Transaksi Tanggal '.$tgl.'</b><br /><br />';
+            echo '<b>Data Penjualan Tanggal '.$tgl.'</b><br /><br />';
             echo '<a href="print.php?filter=1&tanggal='.$_GET['tanggal'].'">Cetak PDF</a><br /><br />';
 
-            $query = "SELECT * FROM pembelian JOIN user ON pembelian.id_user=user.id_user WHERE DATE(tanggal_kirim)='".$_GET['tanggal']."'"; // Tampilkan data transaksi sesuai tanggal yang diinput oleh user pada filter
+            $query = "SELECT * FROM pembelian, user WHERE pembelian.id_user=user.id_user and DATE(tanggal_beli)='".$_GET['tanggal']."'"; // Tampilkan data transaksi sesuai tanggal yang diinput oleh user pada filter
         }else if($filter == '2'){ // Jika filter nya 2 (per bulan)
+            
             $nama_bulan = array('', 'Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','November','Desember');
 
-            echo '<b>Data Transaksi Bulan '.$nama_bulan[$_GET['bulan']].' '.$_GET['tahun'].'</b><br /><br />';
+            echo '<b>Data Penjualan Bulan '.$nama_bulan[$_GET['bulan']].' '.$_GET['tahun'].'</b><br /><br />';
+
             echo '<a href="print.php?filter=2&bulan='.$_GET['bulan'].'&tahun='.$_GET['tahun'].'">Cetak PDF</a><br /><br />';
 
-            $query = "SELECT * FROM pembelian JOIN user ON pembelian.id_user=user.id_user WHERE MONTH(tanggal_kirim)='".$_GET['bulan']."' AND YEAR(tanggal_kirim)='".$_GET['tahun']."'"; // Tampilkan data transaksi sesuai bulan dan tahun yang diinput oleh user pada filter
+            $query = "SELECT * FROM pembelian, user WHERE pembelian.id_user=user.id_user and MONTH(pembelian.tanggal_beli)='".$_GET['bulan']."' AND YEAR(pembelian.tanggal_beli)='".$_GET['tahun']."'"; // Tampilkan data transaksi sesuai bulan dan tahun yang diinput oleh user pada filter
         }else{ // Jika filter nya 3 (per tahun)
-            echo '<b>Data Transaksi Tahun '.$_GET['tahun'].'</b><br /><br />';
+            echo '<b>Data Penjualan Tahun '.$_GET['tahun'].'</b><br /><br />';
             echo '<a href="print.php?filter=3&tahun='.$_GET['tahun'].'">Cetak PDF</a><br /><br />';
 
-            $query = "SELECT * FROM pembelian JOIN user ON pembelian.id_user=user.id_user WHERE YEAR(tanggal_kirim)='".$_GET['tahun']."'"; // Tampilkan data transaksi sesuai tahun yang diinput oleh user pada filter
+            $query = "SELECT * FROM pembelian, user WHERE pembelian.id_user=user.id_user and YEAR(tanggal_periksa)='".$_GET['tahun']."'"; // Tampilkan data transaksi sesuai tahun yang diinput oleh user pada filter
         }
     }else{ // Jika user tidak mengklik tombol tampilkan
-        echo '<b>Semua Data Transaksi</b><br /><br />';
-        echo '<a href="print.php">Cetak PDF</a><br /><br />';
+        echo '<b>Semua Data Penjualan</b><br /><br />';
+        echo '<a href="print.php" class="btn btn-success"><i class="fas fa-print"></i> Cetak PDF</a><br /><br />';
 
-        $query = "SELECT * FROM pembelian JOIN user ON pembelian.id_user=user.id_user ORDER BY tanggal_kirim"; // Tampilkan semua data transaksi diurutkan berdasarkan tanggal
+        $query = "SELECT * FROM pembelian,user WHERE pembelian.id_user=user.id_user ORDER BY tanggal_beli"; // Tampilkan semua data transaksi diurutkan berdasarkan tanggal
     }
     ?>
 
       <table class="table table-bordered"> 
          <thead>  
                <tr>  
-                  <th>Tanggal Kirim</th>
+                  <th>Tanggal Beli</th>
                   <th>Id Pembelian</th>
                   <th>Nama</th>
                     <th>Alamat</th>
@@ -166,6 +186,7 @@ include 'koneksi.php';
                </tr>
          </thead>
          <tbody>  
+            
                <?php
                $totalbelanja=0;
                   $sql = mysqli_query($koneksi, $query); // Eksekusi/Jalankan query dari variabel $query
@@ -174,7 +195,7 @@ include 'koneksi.php';
                   if($row > 0){ // Jika jumlah data lebih dari 0 (Berarti jika data ada)
 
                      while($data = mysqli_fetch_array($sql)){ // Ambil semua data dari hasil eksekusi $sql
-                           $tgl = date('d-m-Y', strtotime($data['tanggal_kirim'])); // Ubah format tanggal jadi dd-mm-yyyy
+                           $tgl = date('d-m-Y', strtotime($data['tanggal_beli'])); // Ubah format tanggal jadi dd-mm-yyyy
                            $subharga=$data['total']+$row['total'];
                            echo "<tr>";
                            echo "<td>".$tgl."</td>";
@@ -276,7 +297,7 @@ include 'koneksi.php';
 
     <!-- Main JS-->
     <script src="js/main.js"></script>
-
+ <?php include 'assets/js.php'; ?>
 </body>
 
 <script>
